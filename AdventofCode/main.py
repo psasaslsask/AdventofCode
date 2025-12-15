4c8206c1244f91aa19a76e5ad446d8a06055d1d5
psasaslsask <palak3438@gmail.com>
2025-12-15 13:05:03 -0500

    solved part two

--- AdventofCode/main.py
+++ AdventofCode/main.py
@@ -1,3 +1,27 @@
+#PART ONE
+
+# with open("input.txt", "r") as file:
+#     start = 50
+#     count = 0
+#     for i in file:
+#         direction = i[0]
+#         distance = int(i[1::])
+#         if direction == "L":
+#             start = (start - distance) % 100
+#             print("The dial is rotated to point", start)
+            
+#         if direction == "R":
+#             start = (start + distance) % 100
+#             print("The dial is rotated to point",start)
+            
+#         if start == 0:
+#             count +=1
+#             print(count)
+        
+# print(count)
+        
+        
+#PART TWO
 with open("input.txt", "r") as file:
     start = 50
     count = 0
@@ -5,16 +29,19 @@ with open("input.txt", "r") as file:
         direction = i[0]
         distance = int(i[1::])
         if direction == "L":
-            start = (start - distance) % 100
-            print("The dial is rotated to point", start)
+            
+            for j in range(distance):
+                start = (start - 1) % 100
+                if start == 0:
+                    count += 1
             
         if direction == "R":
-            start = (start + distance) % 100
-            print("The dial is rotated to point",start)
-            
-        if start == 0:
-            count +=1
-            print(count)
+            for j in range(distance):
+                start = (start + 1) % 100
+                if start == 0:
+                    count += 1
+                
+        
         
 
 print(count)
